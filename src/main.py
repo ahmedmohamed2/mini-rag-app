eef325ecf60c585d8fc243e35a05cf2c54f8d9f5
Ahmed Mohamed <ahmedmohamed1357911@gmail.com>
2026-03-31 15:41:10 +0200

    Upload File

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI
-from dotenv import load_dotenv
 from routes import base
+from routes import data
 
-load_dotenv(".env")
 
 
 app = FastAPI()
 
 
 app.include_router(base.base_router)
+app.include_router(data.data_router)
 
 
